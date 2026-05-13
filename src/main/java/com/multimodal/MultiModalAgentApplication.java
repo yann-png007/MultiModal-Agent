cc5b8110312c69b89888e2c7b6d543a375b66d56
yann-png007 <haoxueyan0415@gmail.com>
2026-05-13 18:44:45 +0800

    day1: 完成 MultiModal-Agent 项目初始化

---
+++ src/main/java/com/multimodal/MultiModalAgentApplication.java
@@ -0,0 +1,11 @@
+package com.multimodal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModalAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModalAgentApplication.class, args);
+    }
+}
